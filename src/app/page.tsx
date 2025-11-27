cdfe384b50556b64ae9267abeca887243bcc6514
Alvaro Pedrozo <alvaropedrozo07@gmail.com>
2025-11-27 04:28:05 -0500

    refactor: standardize quotation marks in imports and improve spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { FamiliesView } from "@/components/families";
-import { getAllFamilies } from "@/services/family";
+import { FamiliesView } from '@/components/families';
+import { getAllFamilies } from '@/services/family';
 
 export default async function Home() {
   const families = await getAllFamilies();
 
   return (
     <div className="min-h-screen bg-zinc-50 dark:bg-zinc-950">
-      <div className="container mx-auto py-10 px-4">
+      <div className="container mx-auto py-10 px-4 ">
         <FamiliesView initialFamilies={families} />
       </div>
     </div>
